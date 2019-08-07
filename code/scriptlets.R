8a27a7312a7509cc0a60ccde478b904914624437
Ted Zhu <tedz2usa@gmail.com>
2019-08-07 14:33:47 -0400

    Refactor code to separate ancillary utility functions.

--- code/scriptlets.R
+++ code/scriptlets.R
@@ -3,37 +3,23 @@
 # This script will include a collection of small scripts steps
 # often seen as example code. We're using this solely for demo purposes
 
+source('./util_functions.R')
+
+# Functions
 
 # 1. Prints hello world
 
-myString <- "Hello, World!"
-
-print (myString)
-
-
-# 2. Square function
-# adapted from https://hbctraining.github.io/Intro-to-R/lessons/03_introR-functions-and-arguments.html#user-defined-functions
-# and https://www.r-bloggers.com/how-to-write-and-debug-an-r-function/
-
-square_it <- function(x){
-  sq <- x*x
-  return(sq)
+hello_world <- function() {
+    myString <- "Hello, World!"
+    print (myString)
 }
 
+
+hello_world()
 square_it(5)
+monte_carlo_pi()
 
 
-# 3. Monte Carlo Pi
-
-for (trials in 1:3000) {
-  count = 0
-  for(i in 1:trials) {
-    if((runif(1,0,1)^2 + runif(1,0,1)^2) < 1) {
-      count = count + 1
-    }
-  }
-  print(paste(trials, ": ", (count*4) / trials))
-}
 
 
 # END
